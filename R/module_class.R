7ac32e4f382ff51995dec44c21991f3809fd7d90
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2021-05-26 13:51:03 -0500

    add validity checks

--- R/module_class.R
+++ R/module_class.R
@@ -41,20 +41,49 @@ setClass("module",
 #' @export
 setClass("assessment_structure",
   slots = c(
-    n_test       = "numeric",
-    n_phase      = "numeric",
-    route_limit_below = "numeric",
-    route_limit_above = "numeric",
+    n_test                    = "numeric",
+    n_phase                   = "numeric",
+    route_limit_below         = "numeric",
+    route_limit_above         = "numeric",
     test_routing_restrictions = "character"
   ),
   prototype = list(
-    n_test       = numeric(0),
-    n_phase      = numeric(0),
-    route_limit_below = numeric(0),
-    route_limit_above = numeric(0),
+    n_test                    = numeric(0),
+    n_phase                   = numeric(0),
+    route_limit_below         = numeric(0),
+    route_limit_above         = numeric(0),
     test_routing_restrictions = character(0)
   ),
   validity = function(object) {
-    return(TRUE)
+    x <- NULL
+    if (object@route_limit_below > 1) {
+      m <- sprintf("currently maat package only supports route_limit_below not larger than 1")
+      x <- c(x, m)
+    }
+    if (object@route_limit_above > 2) {
+      m <- sprintf("currently maat package only supports route_limit_above not larger than 2")
+      x <- c(x, m)
+    }
+    if (object@route_limit_below < 0) {
+      m <- sprintf("route_limit_below must be nonnegative")
+      x <- c(x, m)
+    }
+    if (object@route_limit_above < 0) {
+      m <- sprintf("route_limit_above must be nonnegative")
+      x <- c(x, m)
+    }
+    if (object@n_phase != 2) {
+      m <- sprintf("currently maat package only supports n_phase == 2")
+      x <- c(x, m)
+    }
+    if (object@n_test != 3) {
+      m <- sprintf("currently maat package only supports n_test == 3")
+      x <- c(x, m)
+    }
+    if (length(x) == 0) {
+      return(TRUE)
+    } else {
+      return(x)
+    }
   }
 )
