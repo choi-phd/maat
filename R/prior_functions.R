05df33e6bd066fe73d6ab54b7f6bd0f23e82e8e3
Luping Niu <newl787@gmail.com>
2021-06-11 21:13:15 -0500

    Update prior_functions.R

--- R/prior_functions.R
+++ R/prior_functions.R
@@ -72,3 +72,31 @@ extractPrior <- function(examinee_list, current_module_position) {
   prior_par <- do.call("rbind", prior_par)
   return(prior_par)
 }
+
+#' @noRd
+PriorDistGen <- function(dist_type, prior_par, theta_grid, nj) {
+
+  nq <- length(theta_grid)
+  m  <- NULL
+
+  if (tolower(dist_type) == "normal" && is.vector(prior_par) && length(prior_par) == 2) {
+    x <- dnorm(theta_grid, mean = prior_par[1], sd = prior_par[2])
+    m <- matrix(x, nj, nq, byrow = TRUE)
+  }
+  if (tolower(dist_type) == "normal" && is.matrix(prior_par) && all(dim(prior_par) == c(nj, 2))) {
+    m <- matrix(NA, nj, nq, byrow = TRUE)
+    for (j in 1:nj) {
+      m[j, ] <- dnorm(theta_grid, mean = prior_par[j, 1], sd = prior_par[j, 2])
+    }
+  }
+  if (tolower(dist_type) == "uniform") {
+    x <- 1
+    m <- matrix(x, nj, nq, byrow = TRUE)
+  }
+  if (is.null(m)) {
+    stop("unrecognized 'prior_par': must be a vector c(mean, sd), or a nj * 2 matrix")
+  }
+
+  return(m)
+
+}
